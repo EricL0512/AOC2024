f51c8f3eb2eaf2ec6471d0d9e289f7735f6017eb
EricL0512 <144487690+EricL0512@users.noreply.github.com>
2024-11-30 23:27:19 -0500

    Create Day1.py

---
+++ Day1/Day1.py
@@ -0,0 +1,14 @@
+from typing import TextIO
+
+
+def get_file_data(file_name) -> list[str]:
+    f: TextIO = open(file_name)
+    data: list[str] = []
+    for line in f:
+        data.append(line.rstrip())
+    return data
+
+def part_one() -> None:
+  
+
+file_data = get_file_data("input")
