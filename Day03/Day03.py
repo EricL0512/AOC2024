6886c3fbfbb606c0657f509c9a6d02e2a0bd762b
EricL0512 <144487690+EricL0512@users.noreply.github.com>
2024-12-03 11:22:35 -0500

    Update Day03.py

--- Day03/Day03.py
+++ Day03/Day03.py
@@ -23,14 +23,14 @@ def part_one():
                     second = string.split(",")[1]
                     if first.isnumeric() and second.isnumeric():
                         total += int(first) * int(second)
-    print(total)
+    print(f"part two: {total}")
 
 def part_two():
     all_lines = ""
     total = 0
     for line in file_data:
         all_lines += line
-    all_lines = re.sub(r"don't\(\).+do\(\)", "", all_lines)
+    all_lines = re.sub(r"don't\(\).+?do\(\)", "", all_lines)  # I HATE GREEDY ALGORITHMS
     all_lines = re.sub(r"don't\(\).+", "", all_lines)
     list = all_lines.split("mul")
     list.pop(0)
@@ -42,14 +42,10 @@ def part_two():
                 second = string.split(",")[1]
                 if first.isnumeric() and second.isnumeric():
                     total += int(first) * int(second)
-    print(total)
+    print(f"part one: {total}")
 
 
 
 file_data = get_file_data("input")
-part_two()
 part_one()
-test = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))don't()mul(12,12)"
-test = re.sub(r"don't\(\).+do\(\)", "", test)
-test = re.sub(r"don't\(\).+", "", test,1)
-print(test)
+part_two()
