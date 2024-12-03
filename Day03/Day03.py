dd9df31089696a871bbd8c8ce5908822e5fa2fac
EricL0512 <144487690+EricL0512@users.noreply.github.com>
2024-12-03 08:46:48 -0500

    Update Day03.py

--- Day03/Day03.py
+++ Day03/Day03.py
@@ -1,3 +1,4 @@
+import re
 from typing import TextIO
 
 
@@ -25,43 +26,30 @@ def part_one():
     print(total)
 
 def part_two():
+    all_lines = ""
     total = 0
     for line in file_data:
-        while line.find("don't()") != -1:
-            start_remove = line.find("don't()")
-            end_remove = line.find("do()")
-            print(f"{start_remove} {end_remove}")
-            if start_remove == -1 and end_remove == -1:
-                break
-            elif line.find("do()") == -1 and line.find("don't()") != -1:
-                line = line[:start_remove]
-                continue
-            else:
-                if start_remove < end_remove:
-                    line = line[0:start_remove] + line[end_remove + 4:]
-                    print("s")
-                else:
-                    break
-
-        list = line.split("mul")
-        list.pop(0)
-        for string in list:
-            if string[0] == "(" and ")" in string:
-                string = string[1:string.find(")")]
-                if len(string.split(",")) == 2:
-                    first = string.split(",")[0]
-                    second = string.split(",")[1]
-                    if first.isnumeric() and second.isnumeric():
-                        total += int(first) * int(second)
+        all_lines += line
+    all_lines = re.sub(r"don't\(\).+do\(\)", "", all_lines)
+    all_lines = re.sub(r"don't\(\).+", "", all_lines)
+    list = all_lines.split("mul")
+    list.pop(0)
+    for string in list:
+        if string[0] == "(" and ")" in string:
+            string = string[1:string.find(")")]
+            if len(string.split(",")) == 2:
+                first = string.split(",")[0]
+                second = string.split(",")[1]
+                if first.isnumeric() and second.isnumeric():
+                    total += int(first) * int(second)
     print(total)
-    print("skibigi")
+
 
 
 file_data = get_file_data("input")
-test_str = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-s = test_str.find("don't()")
-e = test_str.find("do()")
-
-test_str1 = test_str[0:s] + test_str[e + 4:]
-print(test_str1)
 part_two()
+part_one()
+test = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))don't()mul(12,12)"
+test = re.sub(r"don't\(\).+do\(\)", "", test)
+test = re.sub(r"don't\(\).+", "", test,1)
+print(test)
