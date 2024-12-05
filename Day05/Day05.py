68297cac331234da8e2ed1883ad027e5210bbacb
EricL0512 <144487690+EricL0512@users.noreply.github.com>
2024-12-05 08:45:50 -0500

    Update Day05.py

--- Day05/Day05.py
+++ Day05/Day05.py
@@ -1,6 +1,6 @@
-import random
-from typing import TextIO
 import itertools
+from typing import TextIO
+
 
 def get_file_data(file_name) -> list[str]:
     f: TextIO = open(file_name)
@@ -25,14 +25,14 @@ def part_one():
             pattern_start = True
         else:
             pattern_list.append(line)
-
+    print(rule_list)
     for pattern in pattern_list:
         correct_pattern = True
         pattern = pattern.split(',')
         for rule in rule_list:
             ahead = rule.split("|")[0]
             behind = rule.split("|")[1]
-            if not(ahead in pattern and behind in pattern):
+            if not (ahead in pattern and behind in pattern):
                 continue
             if pattern.index(behind) < pattern.index(ahead):
                 correct_pattern = False
@@ -40,7 +40,9 @@ def part_one():
             total += int(pattern[len(pattern) // 2])
     print(total)
 
+
 # PART TWO REQUIRES SOME GREEDY ALGORITHM OR INSIGHT
+# INSIGHT FOUND!
 def part_two():
     rule_list = []
     pattern_list = []
@@ -55,7 +57,6 @@ def part_two():
             pattern_start = True
         else:
             pattern_list.append(line)
-
     for pattern in pattern_list:
         correct_pattern = True
         pattern = pattern.split(',')
@@ -73,25 +74,22 @@ def part_two():
     # Yea that's not happening. 2^17 is too much permutations
     """IDEA: Make a list/dict/map based on the rules given? compare the pattern to the 
     list/dict/map and if its in ascending order then its correct? 
-    
+
     To do this, find the front-most number based on the right side of the rule: FRONT MOST
     SHOULD NEVER BE ON THE RIGHT SIDE OF THE RULE AS ITS THE FIRST ELEMENT
-    
+
     NVM this is alot easier than it looks:
     Find all possible values from both sides combined: use a set
     order values by how their mode/frequency of appearance on the right side of the rule
     this will be your guidance list/dict/map that will dictate how the rest of the lists are formed
-    
+
     connect the pattern terms with the guidance list/dict/map 
     O(n)? 
     """
-    
-    
-    print(wrong_patterns)
+
     for line in wrong_patterns:
         pattern_found = False
         permutations = list(itertools.permutations(line))
-        print(permutations)
         for line_two in permutations:
             correct_pattern = True
             for rule in rule_list:
@@ -106,10 +104,54 @@ def part_two():
                 pattern_found = True
             if pattern_found:
                 break
-    print(total)
+
+def redo_two():
+    rule_list = []
+    pattern_list = []
+    ahead_list = []
+    behind_list = []
+    wrong_patterns = []
+    rules_dict: dict = {}
+    pattern_start = False
+    total = 0
+    # Separate the inputs
+    for line in file_data:
+        if line != "" and not pattern_start:
+            rule_list.append(line)
+        elif not pattern_start:
+            pattern_start = True
+        else:
+            pattern_list.append(line)
+    # create the rules list, make the rules dict
+    for rule in rule_list:
+        ahead_list.append(rule.split("|")[0])
+        behind_list.append(rule.split("|")[1])
+    rule_list_nocopy = list(set(ahead_list + behind_list))
+    for number in rule_list:
+        rules_dict[number] = 0
+    for behind in behind_list:
+        if behind in rules_dict:
+            rules_dict[behind] += 1
+
+    # find wrong patterns
+    for pattern in pattern_list:
+        correct_pattern = True
+        pattern = pattern.split(',')
+        for rule in rule_list:
+            ahead = rule.split("|")[0]
+            behind = rule.split("|")[1]
+            if not (ahead in pattern and behind in pattern):
+                continue
+            if pattern.index(behind) < pattern.index(ahead):
+                correct_pattern = False
+        if not correct_pattern:
+            wrong_patterns.append(pattern)
+    correct_patterns = ['']*50  # I believe its 48 or 49 total values but will put 50 to be safe
+    for line in wrong_patterns:
+        for number in line:
+            correct_patterns[rules_dict[number]] = number
+        
 
 
 file_data = get_file_data("input")
-perm = list(itertools.permutations(
-    ['94', '76', '47', '44', '18', '87', '86', '95', '84', '35', '74', '73', '68', '19', '42', '15', '65']))
-print(perm)
+redo_two()
