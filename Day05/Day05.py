1c1ea1ab018aba1f8fca4e45883d6624d1615893
EricL0512 <144487690+EricL0512@users.noreply.github.com>
2024-12-06 22:39:50 -0500

    Update Day05.py

--- Day05/Day05.py
+++ Day05/Day05.py
@@ -25,7 +25,6 @@ def part_one():
             pattern_start = True
         else:
             pattern_list.append(line)
-    print(rule_list)
     for pattern in pattern_list:
         correct_pattern = True
         pattern = pattern.split(',')
@@ -38,76 +37,11 @@ def part_one():
                 correct_pattern = False
         if correct_pattern:
             total += int(pattern[len(pattern) // 2])
-    print(total)
+    print(f"part_one: {total}")
 
-
-# PART TWO REQUIRES SOME GREEDY ALGORITHM OR INSIGHT
-# INSIGHT FOUND!
+# map usage to identify index of a string; then build out the list
+# using a map to sort a list, then taking middle element
 def part_two():
-    rule_list = []
-    pattern_list = []
-    wrong_patterns = []
-    pattern_start = False
-    total = 0
-    # Separate the inputs
-    for line in file_data:
-        if line != "" and not pattern_start:
-            rule_list.append(line)
-        elif not pattern_start:
-            pattern_start = True
-        else:
-            pattern_list.append(line)
-    for pattern in pattern_list:
-        correct_pattern = True
-        pattern = pattern.split(',')
-        for rule in rule_list:
-            ahead = rule.split("|")[0]
-            behind = rule.split("|")[1]
-            if not (ahead in pattern and behind in pattern):
-                continue
-            if pattern.index(behind) < pattern.index(ahead):
-                correct_pattern = False
-        if not correct_pattern:
-            wrong_patterns.append(pattern)
-
-    # Brute force using two for loops
-    # Yea that's not happening. 2^17 is too much permutations
-    """IDEA: Make a list/dict/map based on the rules given? compare the pattern to the 
-    list/dict/map and if its in ascending order then its correct? 
-
-    To do this, find the front-most number based on the right side of the rule: FRONT MOST
-    SHOULD NEVER BE ON THE RIGHT SIDE OF THE RULE AS ITS THE FIRST ELEMENT
-
-    NVM this is alot easier than it looks:
-    Find all possible values from both sides combined: use a set
-    order values by how their mode/frequency of appearance on the right side of the rule
-    this will be your guidance list/dict/map that will dictate how the rest of the lists are formed
-
-    connect the pattern terms with the guidance list/dict/map 
-    
-    DOESNT WORK: RETRY
-    """
-
-    for line in wrong_patterns:
-        pattern_found = False
-        permutations = list(itertools.permutations(line))
-        for line_two in permutations:
-            correct_pattern = True
-            for rule in rule_list:
-                ahead = rule.split("|")[0]
-                behind = rule.split("|")[1]
-                if not (ahead in line_two and behind in line_two):
-                    continue
-                if line_two.index(behind) < line_two.index(ahead):
-                    correct_pattern = False
-            if correct_pattern:
-                total += int(line_two[len(line_two) // 2])
-                pattern_found = True
-            if pattern_found:
-                break
-
-
-def redo_two():
     rule_list = []
     pattern_list = []
     ahead_list = []
@@ -131,10 +65,7 @@ def redo_two():
     rule_list_nocopy = list(set(ahead_list + behind_list))
     for number in rule_list_nocopy:
         rules_dict[number] = 0
-    for behind in behind_list:
-        if behind in rules_dict:
-            rules_dict[behind] += 1
-            print(rules_dict)
+
     # find wrong patterns
     for pattern in pattern_list:
         correct_pattern = True
@@ -149,18 +80,20 @@ def redo_two():
         if not correct_pattern:
             wrong_patterns.append(pattern)
     for line in wrong_patterns:
+        for number in rules_dict:
+            rules_dict[number] = 0
+        for index in range(len(behind_list)):
+            if (behind_list[index] in line) and (ahead_list[index] in line) and (behind_list[index] in rules_dict):
+                rules_dict[behind_list[index]] += 1
         correct_patterns = [''] * 50  # I believe its 49 total values but will put 50 to be safe
         for number in line:
-            print(number)
-            print(rules_dict[number])
             correct_patterns[rules_dict[number]] = number
-            print(correct_patterns)
         temp_list = [i for i in correct_patterns if i != '']
         correct_patterns = temp_list
-        print(correct_patterns)
         total += int(correct_patterns[len(correct_patterns) // 2])
-    print(rules_dict)
+    print(f"part two: {total}")
 
 
 file_data = get_file_data("input")
-redo_two()
+part_one()
+part_two()
