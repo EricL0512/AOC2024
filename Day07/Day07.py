8f229fd51ee8b24a32318739ca7852fc8ea84fcc
EricL0512 <144487690+EricL0512@users.noreply.github.com>
2024-12-07 01:02:33 -0500

    Update Day07.py

--- Day07/Day07.py
+++ Day07/Day07.py
@@ -1,4 +1,5 @@
 from typing import TextIO
+import itertools
 
 
 def get_file_data(file_name) -> list[str]:
@@ -10,11 +11,51 @@ def get_file_data(file_name) -> list[str]:
 
 
 def part_one():
-    pass
+    count = 0
+    for line in file_data:
+        target = int(line.split(":")[0])
+        equation = line.split(":")[1][1:]
+        equation = equation.split(" ")
+        operator_perm = list(itertools.product("*+", repeat=len(equation) - 1))
+        equation2 = list(int(i) for i in equation)
+        equation = equation2
+        for operation in operator_perm:
+            tmp = equation[0]
+            for n, num in enumerate(equation[:-1]):
+                if operation[n] == "*":
+                    tmp = (tmp * equation[n + 1])
+                if operation[n] == "+":
+                    tmp = (tmp + equation[n + 1])
+            if target == tmp:
+                count += target
+                break
+    print(f"part one: {count}")
 
 
 def part_two():
-    pass
+    count = 0
+    for line in file_data:
+        target = int(line.split(":")[0])
+        equation = line.split(":")[1][1:]
+        equation = equation.split(" ")
+        operator_perm = list(itertools.product("*+|", repeat=len(equation) - 1))
+        equation2 = list(int(i) for i in equation)
+        equation = equation2
+        for operation in operator_perm:
+            tmp = equation[0]
+            for n, num in enumerate(equation[:-1]):
+                if operation[n] == "*":
+                    tmp = (tmp * equation[n + 1])
+                if operation[n] == "+":
+                    tmp = (tmp + equation[n + 1])
+                if operation[n] == "|":
+                    tmp = int(str(tmp) + str(equation[n + 1]))
+            if target == tmp:
+                count += target
+                break
+    print(f"part two: {count}")
 
 
 file_data = get_file_data("input")
+part_one()
+part_two()
